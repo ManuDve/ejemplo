1dc6d19bfb7d51ac81beb00ea857f5e3a71b329e
ManuDv <m.alfaro@live.cl>
2023-05-08 23:36:43 -0400

    Archivos iniciales

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,60 +1,40 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <section v-if="nuevasNovelas">
+    <div v-for="novela in nuevasNovelas" :key="novela.id">
+      <h2>{{ novela.title }}</h2>
+      <img :src="novela.thumbnail.path + '.jpg'" alt="">
+      <p>Paginas: {{ novela.pageCount }}</p>
+      <p>ISBN: {{ novela.isbn }}</p>
+      <p><strong>Descripción: </strong> {{ novela.description }}</p>
+      <hr>
+    </div>
+  </section>
 </template>
 
 <script>
+import { MD5 } from "md5-js-tools";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      nuevasNovelas: null
+    }
+  },
+  async created() {
+    try {
+      let marvelApiStart = "https://gateway.marvel.com/v1/public/comics?format=graphic%20novel&apikey=";
+      const marvelPublicKey = '701c9e424072247e27307a909cfab7ae';
+      const marvelPrivateKey = '61397ba16153cf7360d94edd0d6363be33f291ec';
+      let ts = new Date().getTime();
+      let hash = MD5.generate(ts + marvelPrivateKey + marvelPublicKey);
+      let requestUrl = marvelApiStart + marvelPublicKey + "&ts=" + ts + "&hash=" + hash
+      let promesa = await fetch(requestUrl);
+      let datos = await promesa.json();
+      let novelas = await datos.data.results
+      this.nuevasNovelas = novelas;
+      console.log(novelas)
+    } catch (error) {
+      console.log(error)
+    }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
